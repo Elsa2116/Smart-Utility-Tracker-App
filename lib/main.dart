7355c8af51fb5e31661bc78a1c178a68c6b949bb
Elsa2116 <elsialemayehu33@gmail.com>
2026-01-20 10:34:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:smart_utility_tracker/screens/login_screen.dart';
 import 'package:smart_utility_tracker/screens/profile_screen.dart';
 import 'package:smart_utility_tracker/screens/alerts_screen.dart';
 
-import 'package:smart_utility_tracker/screens/app_shell.dart'; // ✅ ADD THIS
+import 'package:smart_utility_tracker/screens/app_shell.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         '/splash': (context) => const SplashScreen(),
         '/login': (context) => const LoginScreen(),
 
-        // ✅ ALL BOTTOM NAVIGATION TABS MUST POINT TO AppShell
+        //  ALL BOTTOM NAVIGATION TABS MUST POINT TO AppShell
         '/home': (context) => const AppShell(initialIndex: 0),
         '/add-reading': (context) => const AppShell(initialIndex: 1),
         '/insights': (context) => const AppShell(initialIndex: 2),
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         '/reminders': (context) => const AppShell(initialIndex: 4),
         '/settings': (context) => const AppShell(initialIndex: 5),
 
-        // ✅ These are NOT tabs, so they can stay as normal pages
+        //  These are NOT tabs, so they can stay as normal pages
         '/profile': (context) => const ProfileScreen(userId: 1),
         '/alerts': (context) => const AlertsScreen(),
       },
